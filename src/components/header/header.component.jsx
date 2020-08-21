4b332963b3238174d12b4e4455b53ccdc4e79a54
Jason Tulloch <jasonbtulloch@gmail.com>
2020-08-21 07:17:06 -0400

    implemented firebase utils, included ability to store authenticated users into our DB with email and password

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -9,7 +9,7 @@ import './header.styles.scss';
 
 const Header = ({ currentUser }) => (
   <div className="header">
-    <Link class="logo-container" to="/">
+    <Link className="logo-container" to="/">
       <Logo className="logo" />
     </Link>
     <div className="options">
